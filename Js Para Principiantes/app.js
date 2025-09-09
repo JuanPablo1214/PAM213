8e4c3602599980688c050fbe63866ad1067cf869
JuanPablo1214 <124048881@upq.edu.mx>
2025-09-09 15:39:36 -0600

    tipos de datos

--- Js Para Principiantes/app.js
+++ Js Para Principiantes/app.js
@@ -1,1 +1,35 @@
-document.write("<h1>Hello World</h1>")
+//tipos de datos
+"Hello world" //String
+'Hello world' //String
+
+//Number
+1000000
+-2.3
+
+// Boolean
+true
+false
+
+//array
+['joe', 'ryan', 'matha']
+[1, 2, 3]
+[true, false, true, false]
+
+//object
+'ryan'
+70.4
+14
+true
+
+{
+    "username": 'ryan',
+    "score": 70.4
+    "hours": 14
+    "proffesional": true
+}
+{
+    "username": 'joe',
+    "score": 0.4
+    "hours": 1
+    "proffesional": false
+}
