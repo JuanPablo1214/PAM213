74d6c8a3ce6a6934490902cbde00754a1a519d56
JuanPablo1214 <124048881@upq.edu.mx>
2025-09-10 18:02:01 -0600

    fin del curso

--- Js Para Principiantes/app.js
+++ Js Para Principiantes/app.js
@@ -5,4 +5,15 @@ function greeting(person){
 
 greeting('markus');
 greeting('kara');
-greeting('connor');
+greeting('markus');
+
+function add(n1,n2){
+    console.log(n1 + n2);
+}
+
+add(3, 2);
+add(100, 200);
+
+add(3, 2);
+add(100, 200);
+add(31665165, 265161);
