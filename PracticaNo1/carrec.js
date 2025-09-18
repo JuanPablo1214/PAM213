6b2a437b45060148de871733bc3c840085307a03
JuanPablo1214 <124048881@upq.edu.mx>
2025-09-17 20:41:35 -0600

    practica1.c

---
+++ PracticaNo1/carrec.js
@@ -0,0 +1,5 @@
+const saludoPersonalizado = (nombre, edad) => `Hola, me llamo ${nombre} y tengo ${edad} años.`;
+
+console.log(saludoPersonalizado("Isay", 37));
+console.log(saludoPersonalizado("Juan", 20));
+console.log(saludoPersonalizado("Ana", 25));
