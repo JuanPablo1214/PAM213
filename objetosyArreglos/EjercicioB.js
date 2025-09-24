bade9bc171d978d03cddaa645c2389aa4ac3daa0
JuanPablo1214 <124048881@upq.edu.mx>
2025-09-23 23:07:06 -0600

    .

---
+++ objetosyArreglos/EjercicioB.js
@@ -0,0 +1,12 @@
+const productos = [
+  { nombre: "Laptop", precio: 12000 },
+  { nombre: "Mouse", precio: 250 },
+  { nombre: "Teclado", precio: 750 },
+  { nombre: "Monitor", precio: 3000 }
+];
+
+const nombres = productos
+  .filter(p => p.precio > 1000)
+  .map(p => p.nombre);
+
+console.log(nombres); 
