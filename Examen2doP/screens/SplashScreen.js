5d8856cb5868b67ff805b0ce1bf8d9bec1a96e4e
JuanPablo1214 <124048881@upq.edu.mx>
2025-11-06 15:32:15 -0600

    Examen

--- Examen2doP/screens/SplashScreen.js
+++ Examen2doP/screens/SplashScreen.js
@@ -1,32 +1,29 @@
 import {view, Buttom, StyleSheet, TouchableOpacity, StatusBar, Image, ImageBackground, Text, View} from 'react-native'
-import { useState, useEffect } from 'react'
+import React,{ useState, useEffect } from 'react'
 
-const MAIN_IMAGE= require('../assets/i.png')
+const MAIN_IMAGE= require('../assets/p.png')
+const SPLASH_IMAGE= require ('../assets/i.png');
 
 export default function SplashScreen(){
-
-    const [screen, setScreen]= useState=('Mi Gáleria');
-        //const [isLoading, setIsLoading]= useState(true);
-
-        switch(screen){
-              case '':
-            return <Screen/>;
-
-            case 'Mi Gáleria':
-            return(
-                <View style={StyleSheet.container}>
-                    <Text style={StyleSheet.title}>Mi Gáleria</Text>
-                        <></>
-
-
-
-
-                </View>
-            );
-
-        
-        }
-
+    const [isloading,setIsloading]= useState(true);
+    useEffect (()=>{
+        const timer = setTimer(()=>{
+            setIsloading(false);
+        },3000);
+        return clearTimeout(timer);
+    }, []);
+    if(isLoading){
+        return(
+            <ImageBackground
+            source={SPLASH_IMAGE}
+            resizeMode='cover'
+            imageStyle= {styles.splashImageStyle}
+            style={styles.splashBackground}
+            >
+            </ImageBackground>
+        )
+    }
+    
           return(
         <ImageBackground
         source={MAIN_IMAGE}
@@ -37,5 +34,35 @@ export default function SplashScreen(){
         </ImageBackground>
     );
 
-    
+
 }
+
+const styles = StyleSheet.create({
+    splashBackground:{
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    },
+    splashImageStyle:{
+    opacity: 0.85,
+    },
+    mainImageStyle:{
+        opacity: 1,
+
+    },
+    mainBackground: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+    //container:{
+    //flex: 1,
+    //justifyContent: 'center',
+    //alignItems: 'center',
+    //backgroundColor: '#7d7d7dff',
+    //},
+});
