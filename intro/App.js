030918e484734471cc0810391c38bdf94521670c
JuanPablo1214 <124048881@upq.edu.mx>
2025-11-13 15:12:47 -0600

    Bottom Sheet Completada

--- intro/App.js
+++ intro/App.js
@@ -1,10 +1,11 @@
-
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import MenuScreen from './screens/MenuScreen';
 
 export default function App() {
   
   return (
+    <GestureHandlerRootView style={{flex:1}}>
     <MenuScreen/>
-
+    </GestureHandlerRootView>
   );
 }
